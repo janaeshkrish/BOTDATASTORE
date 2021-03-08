6a0581739dc507e6f7e327e6ea767b2ab5d374ba
janaeshkrish <janaeshkrish1998@gmail.com>
2021-03-08 09:48:12 +0530

    final

--- actions/actions.py
+++ actions/actions.py
@@ -2,79 +2,103 @@
 # custom Python code.
 #
 # See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List,Union
+from typing import Any, Text, Dict, List, Union
+
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import EventType
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
-from . import excel_read_write
-from rasa_sdk.events import AllSlotsReset
-
+from . import excel
 
 class ActionSaveData(Action):
 
     def name(self) -> Text:
         return "action_save_data"
 
-
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        excel.DataStore(tracker.get_slot("name"),
+            tracker.get_slot("number"),
+            tracker.get_slot("email"),
+            tracker.get_slot("occupation"))
+        dispatcher.utter_message(text="Data Stored Successfully.")
 
-        excel_read_write.DataStore(tracker.get_slot('name'),tracker.get_slot('gender'),tracker.get_slot('city'),tracker.get_slot('occupation'))
-
-        dispatcher.utter_message(text="data stored successfully..")
-
-class FormDataCollect(FormAction):
-
-    def name(self) -> Text:
-        return "Form_Info"
-
-    @staticmethod
-    def required_slots(tracker:"Tracker")-> List[Text]:
-        return ['name','gender','city','occupation']
-
-    def slot_mappings(self) -> Dict[Text,Union[Dict,List[Dict[Text,Any]]]]:
-        return{
-            'name':[self.from_text()],
-            'gender':[self.from_text()],
-            'city':[self.from_text()],
-            'occupation':[self.from_text()],
-        }
-
-    def submit(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        dispatcher.utter_message(text="Here are the information that you are provided would you like to save it?\n Name:{0},\n Gender:{1},\n City:{2},\n Occupation:{3}".format(tracker.get_slot('name'),tracker.get_slot('gender'),tracker.get_slot('city'),tracker.get_slot('occupation')))
-        return[]
-
+        return []
 
 class ActionFetchData(Action):
 
     def name(self) -> Text:
-        return 'action_fetch_data'
+        return "action_fetch_data"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        output=excel.FetchData(tracker.latest_message['entities'][0]['value'],
+                         tracker.latest_message['entities'][1]['value'])
+        dispatcher.utter_message(text="This is the data that you asked for, \n{}".format(",".join(output)))
+
+        return []
+
+class FormDataCollect(FormAction):
+    def name(self) -> Text:
+        return "Form_Info"
+
+    @staticmethod
+    def required_slots(tracker: "Tracker") -> List[Text]:
+        return ["name","number","email","occupation"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+        return {
+            "name":[self.from_text()],
+            "number":[self.from_entity(entity="number")],
+            "email":[self.from_entity(entity="email")],
+            "occupation":[self.from_text()]
+        }
+
+    # def run(self, dispatcher: CollectingDispatcher,
+    #         tracker: Tracker,
+    #         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+    #     dispatcher.utter_message(text="Here are the information that you provided. Do you want to save it?\nName: {0},\nMobile Number: {1},\nEmail: {2},\nOccupation: {3}".format(
+    #         tracker.get_slot("name"),
+    #         tracker.get_slot("number"),
+    #         tracker.get_slot("email"),
+    #         tracker.get_slot("occupation"),
+
+    #     ))
+
+        # dispatcher.utter_message(text="Do you want to save the data?")
+        # dispatcher.utter_message(buttons = [
+        #         {"payload": "/affirm", "title": "Yes"},
+        #         {"payload": "/deny", "title": "No"},
+        #     ])
+        #return []
+
+class DisplayData(Action):
+
+    def name(self) -> Text:
+        return "Display_Data"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        output= excel_read_write.Fetchdata(tracker.latest_message['entities'][0]['value'],tracker.latest_message['entities'][1]['value'])
-        
-        dispatcher.utter_message(text="This the data that you have asked for.\n{}".format(",".join(output)))
+        dispatcher.utter_message(text="Here are the information that you provided. Do you want to save it?\nName: {0},\nMobile Number: {1},\nEmail: {2},\nOccupation: {3}".format(
+            tracker.get_slot("name"),
+            tracker.get_slot("number"),
+            tracker.get_slot("email"),
+            tracker.get_slot("occupation"),
 
-class deleteslots(Action):
+        ))
 
-     def name(self) -> Text:
-            return "slot_clear"
+        return []
 
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    
 
-         return [AllSlotsReset()]
 
---
+++ demo2.py
@@ -0,0 +1,7 @@
+from demo1 import sk
+
+def final():
+    sk.fun()
+
+if __name__== "__main__":
+    final()
